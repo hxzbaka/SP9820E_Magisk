8af586e0f2218eceb1753334b53dbf76cad4b0cc
hxzbaka <165454150+hxzbaka@users.noreply.github.com>
2024-05-25 10:25:26 +0800

    Add files via upload

---
+++ bin/install-recovery_32.sh
@@ -0,0 +1,51 @@
+#!/system/bin/sh
+
+# If you're implementing this in a custom kernel/firmware,
+# I suggest you use a different script name, and add a service
+# to launch it from init.rc
+# By hxzbaka
+# Type 32
+echo 'Start running shell'
+
+echo 'Start install Magisk'
+# Detecting files
+if [[ ! -f /data/adb/magisk/magisk32 ]] ; then
+    echo 'Magisk file does not exist!'
+    exit 0
+fi
+
+#Mount read-write
+echo 'Mount read-write...'
+mount -o rw,remount /
+mount -o rw,remount /system
+
+# Preparation environment
+echo 'Copying files'
+cp -r /data/adb/magisk/* /sbin
+
+#Set up the linked file
+echo 'Set up file'
+mv /sbin/magisk32 /sbin/magisk
+ln -s /sbin/magisk /sbin/resetprop
+ln -s /sbin/magisk /sbin/su
+mkdir /sbin/.magisk/
+mkdir /sbin/.magisk/block
+mkdir /sbin/.magisk/mirror
+mkdir /sbin/.magisk/worker
+touch /sbin/.magisk/config
+ln -s /system /sbin/.magisk/mirror/system
+
+#Set permissions
+echo 'Set permissions'
+chmod 777 /sbin
+chmod 777 /sbin/*
+
+#Start Magisk
+echo 'Start the Magisk daemon'
+/sbin/magisk --post-fs-data
+/sbin/magisk --service
+/sbin/magisk --boot-complete
+/sbin/magisk -v
+
+#Install Magisk End
+echo 'Installation completed'
---
+++ bin/install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+if [[ ! -f /sdcard/type ]] ; then
+    echo "[×] 找不到架构文件，请重新运行脚本！"
+    exit 0
+fi
+result=$(cat /sdcard/type | grep "64")
+echo 'Maigisk安装脚本-1.0'
+if [[ "$result" != "" ]]
+then
+    echo "类型:64位(v8a)"
+    abi=1
+else
+    echo "架构:32位(v7a)"
+    abi=0
+fi
+echo 'By:hxzbaka'
+echo '[*] 获取Root权限...'
+#获取root权限
+result=$(id | grep "root")
+if [[ "$result" != "" ]]
+then
+    echo "[√] 获取Root权限成功！"
+else
+    echo "[×] 获取Root权限失败！请Root后再试"
+    exit 0
+fi
+#判断文件
+echo "[*] 检测Magisk文件是否存在..."
+if [ $abi == "0" ] ; then
+     if [[ ! -f /sdcard/tmp/magisk32 ]] ; then
+     echo '[×] 找不到Magisk文件，请重新运行脚本！'
+     exit 0
+     else
+     echo "[√] Magisk文件校验成功！"
+     fi
+
+    else
+     if [[ ! -f /sdcard/tmp/magisk64 ]] ; then
+     echo '[×] 找不到Magisk文件，请重新运行脚本！'
+     exit 0
+     else
+     echo "[√] Magisk文件校验成功！"
+     fi
+fi
+#挂载读写
+echo "[*] 挂载目录读写权限..."
+echo "[*] 挂载目录:/"
+mount -o rw,remount /
+if [ $? -eq 0 ]; then
+    echo "[√] 挂载成功！"
+else
+    echo "[×] 挂载失败，安装终止！"
+    exit 0
+fi
+echo "[*] 挂载目录:/system"
+mount -o rw,remount /system
+if [ $? -eq 0 ]; then
+    echo "[√] 挂载成功！"
+else
+    echo "[×] 挂载失败，安装终止！"
+    exit 0
+fi
+#复制文件
+echo "[*] 复制文件"
+mkdir /data/adb/magisk
+cp -r /sdcard/tmp/* /data/adb/magisk/
+if [ $? -eq 0 ]; then
+    echo "[√] 复制文件成功！"
+else
+    echo "[×] 复制文件失败，安装终止！"
+    exit 0
+fi
+#安装启动脚本
+echo "[*] 安装启动脚本"
+echo "[*] 检测Magisk文件是否存在..."
+     if [[ ! -f /sdcard/install-recovery.sh ]] ; then
+     echo '[×] 找不到启动脚本，请重新运行脚本！'
+     exit 0
+     else
+     echo "[√] 启动脚本校验成功！"
+     fi
+echo "[*] 开始加载..."
+cp /sdcard/install-recovery.sh /system/etc/install-recovery.sh
+cp /sdcard/install-recovery.sh /system/bin/install-recovery.sh
+chmod 755 /system/etc/install-recovery.sh
+chmod 755 /system/bin/install-recovery.sh
+chown root /system/etc/install-recovery.sh
+chown root /system/bin/install-recovery.sh
+echo "[*] 删除旧su文件..."
+rm /system/bin/su > /dev/null
+rm /system/xbin/su > /dev/null
+rm /system/sbin/su > /dev/null
+echo "[*] 删除残留文件..."
+rm -r /sdcard/tmp
+rm /sdcard/type
+rm /sdcard/install-recovery.sh
+echo "[*] 重启设备..."
+echo "[√] 安装完成！"
+reboot
